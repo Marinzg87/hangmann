3d7b7425e88f5370cbb2168fe57de4486a99a607
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-07-08 09:40:52 +0200

    Step 2 - done

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
 import random
 
-# Step 1
+# Step 1 & 2
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a
+# TODO-1.1 - Randomly choose a word from the word_list and assign it to a
 #  variable called chosen_word.
 chosen_word = random.choice(word_list)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a
-#  variable called guess. Make guess lowercase.
-user_guess = input("Guess a letter: ")
-guess = user_guess.lower()
+# Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters
+# TODO-2.1: - Create an empty List called display.
+#  For each letter in the chosen_word, add a "_" to 'display'.
+#  So if the chosen_word was "apple", display should be ["_", "_", "_", "_",
+#  "_"] with 5 "_" representing each letter to guess.
+display = ""
+for character in chosen_word:
+    display += "-"
+display = list(display)
+
+# TODO-1.2 - Ask the user to guess a letter and assign their answer to a
+#  variable called guess. Make guess lowercase.
+guess = input("Guess a letter: ").lower()
+
+# TODO-2.2: - Loop through each position in the chosen_word;
+#  If the letter at that position matches 'guess' then reveal that letter in
+#  the display at that position.
+#  e.g. If the user guessed "p" and the chosen word was "apple", then display
+#  should be ["_", "p", "p", "_", "_"].
+counter = 0
+for letter in chosen_word:
+    if letter == guess:
+        display[counter] = guess
+    counter += 1
+
+# TODO-1.3 - Check if the letter the user guessed (guess) is one of the letters
 #  in the chosen_word.
-for char in chosen_word:
-    if guess == char:
-        print("Correct")
-    else:
-        print("Wrong")
+# for letter in chosen_word:
+#     if letter == guess:
+#         print("Correct")
+#     else:
+#         print("Wrong")
+
+# TODO-2.3: - Print 'display' and you should see the guessed letter in the
+#  correct position and every other letter replace with "_".
+#  Hint - Don't worry about getting the user to guess the next letter. We'll
+#  tackle that in step 3.
+print(display)
+print(type(display))
