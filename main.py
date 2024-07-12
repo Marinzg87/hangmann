2dc93b983bb63defadff7ae86c96d6fe64928d5d
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-07-12 08:46:11 +0200

    Step 5 - improve useer experience. Added logo, added info that user already guessed the letter, added info that guessed letter is wrong and user lost one life. Added list of words.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import random
 from ascii_art import stages
-
-word_list = ["aardvark", "baboon", "camel"]
+from words import word_list
+from ascii_art import logo
 
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
+print(logo)
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
 for _ in range(word_length):
     display += "_"
-print(display)
+# print(display)
 
 end_of_game = False
 lives = 6
@@ -19,16 +20,22 @@ lives = 6
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
+    if guess in display:
+        print(f"You've already guessed {guess}.")
+
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = guess
+
     if guess not in display:
         lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         if lives == 0:
             end_of_game = True
             print("You lose!")
-    print(display)
+
+    print(f"{' '.join(display)}")
 
     if "_" not in display:
         end_of_game = True
